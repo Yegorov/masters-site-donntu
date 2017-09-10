4fba52e212ae1dfd387972a30f9ba563c6b7ea5a
Yegorov <yegorov0725@yandex.ru>
2017-09-10 13:36:53 +0400

    Init

---
+++ README.md
@@ -0,0 +1,18 @@
+Server for masters site DonNTU
+==============================
+
+## How use
+
+```bash
+# install nodejs and npm
+$ git clone git@github.com:Yegorov/masters-site-donntu.git
+$ cd masters-site-server-donntu
+$ npm install
+$ node ./server.js
+# Open url http://localhost:3000
+# Change the contents of the files in the public folder
+```
+
+The site template is taken from [masters.donntu.org](http://masters.donntu.org/template/index.htm)
+
+License (MIT) Copyright (c) 2017 Yegorov A. <yegorov0725@yandex.ru>
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var serveStatic = require('serve-static');
+
+var app = express();
+
+function setHeaders(res, path) {
+    if (serveStatic.mime.lookup(path) === 'text/html') {
+        res.setHeader('Content-Type', "text/html; charset=windows-1251");
+        // Custom Cache-Control for HTML files
+        // res.setHeader('Cache-Control', 'public, max-age=0')
+        res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
+    }
+}
+
+app.use(serveStatic('public', {
+        'index': ['index.htm'],
+        'setHeaders': setHeaders
+    }));
+
+app.listen(3000)
